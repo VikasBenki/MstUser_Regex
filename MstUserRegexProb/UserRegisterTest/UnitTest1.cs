b012f638ea73f4a3dcb83079dfe287e8bed07ae7
VikasBenki <vikasmsbenki@gmail.com>
2022-02-14 19:55:48 +0530

    [VikasBenki] adding exception handling

--- MstUserRegexProb/UserRegisterTest/UnitTest1.cs
+++ MstUserRegexProb/UserRegisterTest/UnitTest1.cs
@@ -8,14 +8,16 @@ namespace UserRegisterTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestFirstName_ReturnTrue()
+        [TestCategory("FirstName_ReturnTrue")]
+        public void GivenFirstNameShouldReturnTrue()
         {
             string excepted = "true";
             var actual = UserRegister.ValidateFirstName("Saurav");
             Assert.AreEqual(excepted, actual);
         }
         [TestMethod]
-        public void TestFirstName_ReturnFalse()
+        [TestCategory("FirstName_ReturnFalse")]
+        public void GivenFirstNameShouldReturnFalse()
         {
             string excepted = "false";
             var actual = UserRegister.ValidateFirstName("Sa");
@@ -24,30 +26,52 @@ namespace UserRegisterTest
             Assert.AreEqual(excepted, actual1);
         }
         [TestMethod]
-        public void TestLastName_ReturnTrue()
+        [TestCategory("LastName_ReturnTrue")]
+        public void GivenLastNameShouldReturnTrue()
         {
             string excepted = "true";
-            var actual = UserRegister.ValidateFirstName("Kumar");
+            var actual = UserRegister.ValidateLastName("Kumar");
             Assert.AreEqual(excepted, actual);
         }
         [TestMethod]
-        public void TestLastName_ReturnFalse()
+        [TestCategory("LastName_ReturnFalse")]
+        public void GivenLastNameShouldReturnFalse()
         {
             string excepted = "false";
-            var actual = UserRegister.ValidateFirstName("Ka");
+            var actual = UserRegister.ValidateLastName("Ka");
             Assert.AreEqual(excepted, actual);
-            var actual1 = UserRegister.ValidateFirstName("kumar");
+            var actual1 = UserRegister.ValidateLastName("kumar");
             Assert.AreEqual(excepted, actual1);
         }
         [TestMethod]
-        public void TestMobileNumber_ReturnTrue()
+        [TestCategory("EmailId_ReturnTrue")]
+        public void GivenEmailIDshouldReturnTrue()
+        {
+            string excepted = "true";
+            var actual = UserRegister.ValidateEmailID("saurav@gmail.co.in");
+            Assert.AreEqual(excepted, actual);
+        }
+        [TestMethod]
+        [TestCategory("EmailId_ReturnFalse")]
+        public void GivenEmailIDShouldReturnFalse()
+        {
+            string excepted = "false";
+            var actual = UserRegister.ValidateEmailID("saurav.kr@g.in");
+            Assert.AreEqual(excepted, actual);
+            var actual1 = UserRegister.ValidateEmailID(".kr@gmail.com.in");
+            Assert.AreEqual(excepted, actual1);
+        }
+        [TestMethod]
+        [TestCategory("MobileNumber_ReturnTrue")]
+        public void GivenMobileNumberShouldReturnTrue()
         {
             string excepted = "true";
             var actual = UserRegister.ValidateMobileNumber("91 7067845485");
             Assert.AreEqual(excepted, actual);
         }
         [TestMethod]
-        public void TestMobileNumber_ReturnFalse()
+        [TestCategory("MobileNumber_ReturnFalse")]
+        public void GivenMobileNumberShouldReturnFalse()
         {
             string excepted = "false";
             var actual = UserRegister.ValidateMobileNumber("7067845485");
@@ -56,6 +80,22 @@ namespace UserRegisterTest
             Assert.AreEqual(excepted, actual1);
             var actual2 = UserRegister.ValidateMobileNumber("91 70678454858");
             Assert.AreEqual(excepted, actual2);
+        }
+        [TestMethod]
+        [TestCategory("Password_ReturnTrue")]
+        public void GivenPasswordShouldReturnTrue()
+        {
+            string excepted = "true";
+            var actual = UserRegister.ValidatePassword("SauGav76@#$");
+            Assert.AreEqual(excepted, actual);
+        }
+        [TestMethod]
+        [TestCategory("Password_ReturnFalse")]
+        public void GivenPasswordShouldReturnFalse()
+        {
+            string excepted = "false";
+            var actual = UserRegister.ValidatePassword("S9a@#$j");
+            Assert.AreEqual(excepted, actual);
         }
     }
 }
